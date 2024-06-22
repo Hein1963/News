60fec23e1adf5a827639648aa1edf4fb31999139
Hein Min <11897.coder@gmail.com>
2024-06-23 02:10:44 +0630

    laravel blade

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -7,8 +7,8 @@
     <link href="/app.css" rel="stylesheet">
 </head>
 <body>
-    <h1><?= $blog->title; ?></h1>
-    <p><?= $blog->body; ?></p>
+    <h1>{{ $blog->title }}</h1>
+    <p>{!! $blog->body !!}</p>
     <a href="/">go back</a>
     <script src="/app.js"></script>
 </body>
--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -7,19 +7,21 @@
     <link href="/app.css" rel="stylesheet">
 </head>
 <body>
-    <?php foreach($blogs as $blog): ?>
-        <h1><a href="blogs/<?= $blog->slug; ?>"><?= $blog->title; ?></a></h1>
+    @foreach($blogs as $blog)
+        <h1><a href="blogs/{{ $blog->slug }}">
+                {{ $blog->title }}
+        </a></h1>
         
         <div>
             <p>
                 published at-
-                <?= $blog->date; ?>
+                {{ $blog->date }}
             </p>
             <p>
-                <?= $blog->intro; ?>
+                {{ $blog->intro }}
             </p>
         </div>
-    <?php endforeach ?>
+    @endforeach
     <script src="/app.js"></script>
 </body>
 </html>
