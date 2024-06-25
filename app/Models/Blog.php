0d4b25e478c3dacc92eb5766b06173c9748b5f8e
Hein Min <11897.coder@gmail.com>
2024-06-25 14:14:31 +0630

    handling mass-assignment

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -1,45 +1,16 @@
-<?php 
+<?php
 
-    namespace App\Models;
-    use Illuminate\Support\Facades\File;
-    use Spatie\YamlFrontMatter\YamlFrontMatter;
+namespace App\Models;
 
-    class Blog
-    {
-        public $title;
-        public $slug;
-        public $intro;
-        public $body;
-        public $date;
-        public function __construct($title,$slug,$intro,$body,$date)
-        {
-            $this->title = $title;
-            $this->slug = $slug;
-            $this->intro = $intro;
-            $this->body = $body;
-            $this->date = $date;
-        }
-        public static function all()
-        {
-            return collect(File::files(resource_path("blogs")))
-                    ->map(function ($file)
-            {
-                $obj = YamlFrontMatter::parseFile($file);
-                return new Blog($obj->title,$obj->slug,$obj->intro,$obj->body(),$obj->date);
-            })->sortByDesc('date');
-        }
-        public static function find($slug)
-        {
-            $blogs = static::all();
-            return $blogs->firstWhere('slug',$slug);
-        }
-        public static function findOrFail($slug)
-        {
-            $blog =static::find($slug);
-            if(!$blog){
-                abort(404);
-            }
-            return $blog;
-        }
-    }
-?>
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    use HasFactory;
+    protected $fillable=[
+        'title',
+        'intro',
+        'body'
+    ];
+}
--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -3,14 +3,14 @@
         <title>All Blogs</title>
     </x-slot>
         @foreach($blogs as $blog)
-                <h1><a href="blogs/{{ $blog->slug }}">
+                <h1><a href="blogs/{{ $blog->id }}">
                         {{ $blog->title }}
                 </a></h1>
                 
                 <div>
                     <p>
                         published at-
-                        {{ $blog->date }}
+                        {{ $blog->created_at->diffForHumans() }}
                     </p>
                     <p>
                         {{ $blog->intro }}
